8baa39a36fb3c5916a7271f42442674e7b4ff38e
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-07 20:50:34 -0500

    armando plantilla inicial

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientes-app';
+
+  curso = "Angular y NetCore";
+
+  profesor = "Jose Diaz";
+
+
 }
--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  { path: 'clientes', component: AppComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
