c065f4dbda1776fd8ac4f7dc062ef3c51bdd2de6
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-07 21:07:01 -0500

    bootstrap  y jquery agregados al proyecto

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  { path: 'clientes', component: AppComponent},
+  { path: 'clientes', component: ClientesComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
