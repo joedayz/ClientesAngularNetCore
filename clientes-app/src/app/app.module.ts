0732496b0609d9e8a7c6f4206eea4b204f0f9413
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-09 21:20:34 -0500

    listato de clientes

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/clientes.service';
+import { FormComponent } from './clientes/form.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -13,6 +15,8 @@ import { HeaderComponent } from './header/header.component';
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full'},
   { path: 'clientes', component: ClientesComponent},
+  { path: 'clientes/form', component: FormComponent},
+  { path: 'clientes/form/:id', component: FormComponent},
 ];
 
 @NgModule({
@@ -20,11 +24,13 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
--- clientes-app/src/app/clientes/clientes.component.ts
+++ clientes-app/src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './clientes.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
     selector: 'app-clientes',
@@ -21,5 +22,32 @@ export class ClientesComponent implements OnInit {
             clientes => this.clientes = clientes
         );
 
+    }
+
+
+    delete(cliente: Cliente): void{
+        Swal.fire({
+            title: 'Are you sure want to remove?',
+            text: 'You will not be able to recover this file!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, keep it'
+          }).then((result) => {
+            if (result.value) {
+              Swal.fire(
+                'Deleted!',
+                'Your imaginary file has been deleted.',
+                'success'
+              )
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+              Swal.fire(
+                'Cancelled',
+                'Your imaginary file is safe :)',
+                'error'
+              )
+            }
+          })
+       
     }
 }
