56b7427a9aa1018918373461304237e88460983d
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-04-12 20:54:54 -0500

    clean obj, bin

--- clientes-app/src/app/clientes/cliente.ts
+++ clientes-app/src/app/clientes/cliente.ts
@@ -1,11 +1,13 @@
 import { Region } from "./region";
 
+
 export class Cliente {
-    id: number;
-    nombre: string;
-    apellido: string;
-    createAt: string;
-    email: string;
-    foto: string;
-    region: Region;
+  id: number;
+  nombre: string;
+  apellido: string;
+  createAt: string;
+  email: string;
+  foto: string;
+  region: Region;
+
 }
